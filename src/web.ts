f10a33b8404095386a3c2dd8ea5f24431e129002
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2022-09-29 00:10:18 -0300

    Migrate file to use typescript

---
+++ src/web.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from 'express'
+import { AddressInfo } from 'net'
+import bodyParser from 'body-parser'
+import packageInfo from '../package.json' assert { type: "json" };
+
+const port = process.env.PORT || '80'
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', function (_: Request, res: Response) {
+  res.json({ version: packageInfo.version })
+})
+
+let server = app.listen(parseInt(port), '0.0.0.0', () => {
+  let serverInfo = server.address()
+  if (isAddressInfo(serverInfo)) {
+    console.log('Web server started at http://%s:%s', serverInfo.address, serverInfo.port)
+  }
+})
+
+function isAddressInfo(info: string | AddressInfo | null): info is AddressInfo {
+  return (info as AddressInfo).address !== undefined
+}
+
+export default (bot: any) => {
+  app.post('/' + bot.token, (req: Request, res: Response) => {
+    bot.processUpdate(req.body)
+    res.sendStatus(200)
+  })
+}
