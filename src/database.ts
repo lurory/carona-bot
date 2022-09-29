08e20b0a03b95891e44faa96db218426657e5369
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2022-09-29 20:34:12 -0300

    Refactor database client to turn it into a class

--- src/database.ts
+++ src/database.ts
@@ -1,82 +1,67 @@
-import * as mongoDB from 'mongodb'
+import { MongoClient, Collection, Db, InsertOneResult, Filter, Document } from 'mongodb'
 import { MONGO_URL, MONGO_COLLECTION_NAME } from '../utils/const.js'
-import { Group } from '../typings/ride.js'
+import { Group, GroupRides } from '../typings/ride.js'
 
-export const collections: { rides?: mongoDB.Collection } = {}
-const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGO_URL)
+export default class Database {
+  _client: MongoClient
+  _collection: Collection
 
-export async function connectToDatabase() {
-  await client.connect((err: any, _: any) => {
-    if (err) throw err
-    console.log('Connected to the MongoDB')
-  })
-
-  const db: mongoDB.Db = client.db(process.env.DB_NAME)
-
-  const ridesCollection: mongoDB.Collection = db.collection(MONGO_COLLECTION_NAME)
-
-  collections.rides = ridesCollection
-
-  console.log(
-    `Successfully connected to database: ${db.databaseName} and collection: ${ridesCollection.collectionName}`
-  )
-}
-
-export async function closeConnection() {
-  await client.close((err: any, _: any) => {
-    if (err) throw err
-    console.log('Closed the MongoDB connection')
-  })
-}
-
-export async function scrapeGroupRides(chatId: number): Promise<unknown> {
-  return await collections.rides?.find({ chatId: chatId }).toArray()
-}
-
-export async function getRide(filter: mongoDB.Filter<mongoDB.Document>): Promise<unknown[]>
-  const document = await collections.rides?.find(filter).toArray()
-
-  if (!document) {
-    throw Error('not found')
+  constructor() {
+    this._client = new MongoClient(MONGO_URL)
+    this._collection = new Collection()
   }
 
-  return document
-}
+  connect = () => {
+    this._client.connect((err: any, _: any) => {
+      if (err) throw err
+      console.log('Connected to the MongoDB')
+    })
 
-export async function createGroup(
-  newGroup: Group
-): Promise<mongoDB.InsertOneResult<mongoDB.Document> | undefined> {
-  const result = await collections.rides?.insertOne(newGroup)
+    const db: Db = this._client.db(process.env.DB_NAME)
 
-  if (!result) {
-    throw Error('not found')
+    const ridesCollection: Collection = db.collection(MONGO_COLLECTION_NAME)
+
+    this._collection = ridesCollection
+
+    console.log(
+      `Successfully connected to database: ${db.databaseName} and collection: ${ridesCollection.collectionName}`
+    )
   }
 
-  return result
-}
+  disconnect = () =>
+    this._client.close((err: any, _: any) => {
+      if (err) throw err
+      console.log('Closed the MongoDB connection')
+    })
 
-export async function updateGroup(
-  chatId: number,
-  mutation: Partial<mongoDB.Document>,
-  options: {
-    upsert: boolean
+  scrapeGroupRides = (chatId: number): Promise<GroupRides> =>
+    this._collection.find({ chatId: chatId }).toArray() as Promise<unknown> as Promise<GroupRides>
+
+  getRide = async (filter: Filter<Document>): Promise<unknown[]> => {
+    const document = await this._collection.find(filter).toArray()
+
+    if (!document) {
+      throw Error('not found')
+    }
+
+    return document
   }
-): Promise<boolean> {
-  let wasMofidied = false
-  let result = await collections.rides?.updateOne({ chatId: chatId }, mutation, options)
-  wasMofidied = (result?.modifiedCount as number) > 0
-  console.log(result?.modifiedCount + ' element(s) modified.')
-  return wasMofidied
-}
 
-export async function cleanRides(
-  params: {
-    chatId: number
-    direction: string
-    now: Date
-  },
-  mutation: Partial<mongoDB.Document>
-) {
-  let result = await collections.rides?.updateOne({ chatId: params.chatId }, mutation)
-  console.log(result?.modifiedCount + ' element(s) modified.')
+  createGroup = (newGroup: Group): Promise<InsertOneResult<Document>> =>
+    this._collection.insertOne(newGroup)
+
+  updateGroup = async (
+    chatId: number,
+    mutation: Partial<Document>,
+    options: {
+      upsert: boolean
+    }
+  ): Promise<boolean> => {
+    let wasMofidied = false
+    let result = await this._collection.updateOne({ chatId: chatId }, mutation, options)
+    wasMofidied = (result?.modifiedCount as number) > 0
+    console.log(result?.modifiedCount + ' element(s) modified.')
+
+    return wasMofidied
+  }
 }
