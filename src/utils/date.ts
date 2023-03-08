06c1b2c3cecb9176bce78dc81f1f4ebcca68eb2a
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2023-03-08 00:09:05 -0300

    hotfix: disable locale timezone
    This was breaking while comparing the times. We worked on a hotfix, but we'll need to have a config per group to define its timezone and correctly process the hours, having UTC as the default one

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,11 @@
 import { timeRegexPattern } from './const.js'
 
 export const getCurrentTime = () => {
-  const dateStr = new Date().toLocaleString('en-US', { timeZone: 'America/Sao_Paulo' })
-  //Fixing daylight saving time bug for now
-  // now.setHours(now.getHours() - 1)
+  const d = new Date()
 
-  return new Date(dateStr)
+  //TODO: Remove hardcoded Brasilia timezone
+  d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000 - 3 * 60 * 60 * 1000)
+  return new Date(d)
 }
 
 export const validateTimeFormat = (time: string) => {
